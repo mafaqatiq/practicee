f0ce7fd787324edf78a02c2cd0027b8055179500
Afaq <168016382+mafaqatiq@users.noreply.github.com>
2024-07-25 20:14:55 +0500

    master cipy changes

--- lib/master_copy/master_copy.dart
+++ lib/master_copy/master_copy.dart
@@ -10,6 +10,6 @@ class MasterCopy extends StatefulWidget {
 class _MasterCopyState extends State<MasterCopy> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container();
   }
 }
